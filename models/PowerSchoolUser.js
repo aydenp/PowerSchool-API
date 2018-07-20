55970604ee58a443c487c5e5c9006b698dcbbc9d
Ayden Panhuyzen <aydenp@users.noreply.github.com>
2018-07-20 08:39:06 -0400

    Multi-user and test credential file support

--- models/PowerSchoolUser.js
+++ models/PowerSchoolUser.js
@@ -33,14 +33,14 @@ class PowerSchoolUser {
         this.userID = this.session.userId;
         this.userType = this.session.userType;
         // We need to fetch these separately
-        this.studentData = new PowerSchoolStudentInfo();
+        this.studentData = [];
     }
 
     /**
-     * Get information about this student.
-     * @return {Promise.<PowerSchoolStudentInfo, Error>} A promise that resolves with the user's student info, and rejects with an Error if one occurred.
+     * Get information about this account's student(s).
+     * @return {Promise.<PowerSchoolStudentInfo[], Error>} A promise that resolves with the account's students information, and rejects with an Error if one occurred.
      */
-    getStudentInfo() {
+    getStudentsInfo() {
         return new Promise((resolve, reject) => {
             var data = {
                 userSessionVO: {
@@ -58,57 +58,80 @@ class PowerSchoolUser {
                 }
             }
             this.api.client.getStudentData(data, this.api.requestOptions, (err, result) => {
-                if(!result || !result.return || !result.return.studentDataVOs) return reject(err);
-                var data = result.return.studentDataVOs;
+                if (!result || !result.return || !result.return.studentDataVOs) return reject(err);
+                var studentsData = this.safelyParseUnpredictableArray(result.return.studentDataVOs);
 
-                // Deserialize any data we might need for special types
-                var schools = (typeof data.schools === "array" ? data.schools : [data.schools]).map((data) => PowerSchoolSchool.fromData(data, this.api)); // for some reason sometimes is an array, sometimes is one school.
-                var teachers = data.teachers.map((data) => PowerSchoolTeacher.fromData(data));
-                var terms = data.terms.map((data) => PowerSchoolTerm.fromData(data, this.api));
-                var reportingTerms = data.reportingTerms.map((data) => PowerSchoolReportingTerm.fromData(data, this.api));
-                var assignments = data.assignments.map((data) => PowerSchoolAssignment.fromData(data, this.api));
-                var assignmentScores = data.assignmentScores.map((data) => PowerSchoolAssignmentScore.fromData(data, this.api));
-                var attendanceCodes = data.attendanceCodes.map((data) => PowerSchoolAttendanceCode.fromData(data, this.api));
-                var periods = data.periods.map((data) => PowerSchoolPeriod.fromData(data, this.api));
-                var courses = data.sections.map((data) => PowerSchoolCourse.fromData(data, this.api));
-                var finalGrades = data.finalGrades.map((data) => PowerSchoolFinalGrade.fromData(data, this.api));
+                studentsData.forEach((data) => {
+                    var studentData = new PowerSchoolStudentInfo();
 
-                // Add assignments to their categories
-                var assignmentCategories = {};
-                data.assignmentCategories.forEach((data) => assignmentCategories[data.id] = PowerSchoolAssignmentCategory.fromData(data, this.api));
-                assignments.filter((a) => assignmentCategories[a.categoryID]).forEach((a) => assignmentCategories[a.categoryID].assignments.push(a));
+                    // Deserialize any data we might need for special types
+                    var schools = [];
+                    if (data.schools) schools = this.safelyParseUnpredictableArray(data.schools).map((data) => PowerSchoolSchool.fromData(data, this.api)); // for some reason sometimes is an array, sometimes is one school.
+                    var teachers = this.safelyParseUnpredictableArray(data.teachers).map((data) => PowerSchoolTeacher.fromData(data));
+                    var terms = this.safelyParseUnpredictableArray(data.terms).map((data) => PowerSchoolTerm.fromData(data, this.api));
+                    var reportingTerms = this.safelyParseUnpredictableArray(data.reportingTerms).map((data) => PowerSchoolReportingTerm.fromData(data, this.api));
+                    var assignments = this.safelyParseUnpredictableArray(data.assignments).map((data) => PowerSchoolAssignment.fromData(data, this.api));
+                    var assignmentScores = this.safelyParseUnpredictableArray(data.assignmentScores).map((data) => PowerSchoolAssignmentScore.fromData(data, this.api));
+                    var attendanceCodes = this.safelyParseUnpredictableArray(data.attendanceCodes).map((data) => PowerSchoolAttendanceCode.fromData(data, this.api));
+                    var periods = this.safelyParseUnpredictableArray(data.periods).map((data) => PowerSchoolPeriod.fromData(data, this.api));
+                    var courses = this.safelyParseUnpredictableArray(data.sections).map((data) => PowerSchoolCourse.fromData(data, this.api));
+                    var finalGrades = this.safelyParseUnpredictableArray(data.finalGrades).map((data) => PowerSchoolFinalGrade.fromData(data, this.api));
 
-                // Store information needed for other data mappings
-                this.api.storeCacheInfo(teachers, "teachers");
-                this.api.storeCacheInfo(schools, "schools", "schoolNumber");
-                this.api.storeCacheInfo(periods, "periods");
-                this.api.storeCacheInfo(courses, "courses");
-                this.api.storeCacheInfo(finalGrades, "finalGrades", "courseID");
-                this.api.storeCacheInfo(terms, "terms");
-                this.api.storeCacheInfo(reportingTerms, "reportingTerms");
-                this.api.storeCacheInfo(Object.values(assignmentCategories), "assignmentCategories");
-                this.api.storeCacheInfo(assignments, "assignments");
-                this.api.storeCacheInfo(assignmentScores, "assignmentScores", "assignmentID");
-                this.api.storeCacheInfo(attendanceCodes, "attendanceCodes");
+                    // Add assignments to their categories
+                    var assignmentCategories = {};
+                    this.safelyParseUnpredictableArray(data.assignmentCategories).forEach((data) => assignmentCategories[data.id] = PowerSchoolAssignmentCategory.fromData(data, this.api));
+                    assignments.filter((a) => assignmentCategories[a.categoryID]).forEach((a) => assignmentCategories[a.categoryID].assignments.push(a));
 
-                // Store the rest of the data for use in the student model
-                this.studentData.schools = schools;
-                this.studentData.teachers = teachers;
-                this.studentData.periods = periods;
-                this.studentData.courses = courses;
-                this.studentData.terms = terms;
-                this.studentData.reportingTerms = reportingTerms;
-                this.studentData.notInSessionDays = data.notInSessionDays.map((data) => PowerSchoolEvent.fromData(data, this.api));
-                this.studentData.student = PowerSchoolStudent.fromData(data.student, this.api);
-                this.studentData.yearID = data.yearId;
-                this.studentData.assignmentCategories = Object.values(assignmentCategories);
-                this.studentData.attendanceRecords = data.attendance.map((data) => PowerSchoolAttendanceRecord.fromData(data, this.api));
-                this.studentData.attendanceCodes = attendanceCodes;
-                this.studentData.finalGrades = finalGrades;
+                    // Store information needed for other data mappings
+                    this.api.storeCacheInfo(teachers, "teachers");
+                    this.api.storeCacheInfo(schools, "schools", "schoolNumber");
+                    this.api.storeCacheInfo(periods, "periods");
+                    this.api.storeCacheInfo(courses, "courses");
+                    this.api.storeCacheInfo(finalGrades, "finalGrades", "courseID");
+                    this.api.storeCacheInfo(terms, "terms");
+                    this.api.storeCacheInfo(reportingTerms, "reportingTerms");
+                    this.api.storeCacheInfo(Object.values(assignmentCategories), "assignmentCategories");
+                    this.api.storeCacheInfo(assignments, "assignments");
+                    this.api.storeCacheInfo(assignmentScores, "assignmentScores", "assignmentID");
+                    this.api.storeCacheInfo(attendanceCodes, "attendanceCodes");
+
+                    // Store the rest of the data for use in the student model
+                    studentData.schools = schools;
+                    studentData.teachers = teachers;
+                    studentData.periods = periods;
+                    studentData.courses = courses;
+                    studentData.terms = terms;
+                    studentData.reportingTerms = reportingTerms;
+                    studentData.notInSessionDays = this.safelyParseUnpredictableArray(data.notInSessionDays).map((data) => PowerSchoolEvent.fromData(data, this.api));
+                    studentData.student = PowerSchoolStudent.fromData(data.student, this.api);
+                    studentData.yearID = data.yearId;
+                    studentData.assignmentCategories = Object.values(assignmentCategories);
+                    studentData.attendanceRecords = this.safelyParseUnpredictableArray(data.attendance).map((data) => PowerSchoolAttendanceRecord.fromData(data, this.api));
+                    studentData.attendanceCodes = attendanceCodes;
+                    studentData.finalGrades = finalGrades;
+
+                    this.studentData.push(studentData);
+                });
 
                 resolve(this.studentData);
             });
         });
+    }
+    
+    safelyParseUnpredictableArray(arr) {
+        if (!arr) return [];
+        if (Array.isArray(arr)) return arr;
+        return [arr];
+    }
+
+    /**
+     * Get the information about the first student on the account.
+     * 
+     * @deprecated Use `getStudentInfo()` to better support multi-user accounts.
+     * @return {Promise.<PowerSchoolStudentInfo, Error>} A promise that resolves with the user's student info, and rejects with an Error if one occurred.
+     */
+    async getStudentInfo() {
+        return (await this.getStudentsInfo())[0];
     }
 }
 
--- test.js
+++ test.js
@@ -1,10 +1,16 @@
 const PowerSchoolAPI = require("./index");
 const readline = require("readline-sync");
 
+// try to load test credentials from file if exists
+var testCredentials = {};
+try {
+    testCredentials = require("./test-credentials.json");
+} catch (e) {}
+
 // Get information from the user.
-const url = readline.question("Enter the PowerSchool installation URL (such as: http://sales.powerschool.com): ");
-const username = readline.question("Enter your PowerSchool username: ");
-const password = readline.question("Enter your PowerSchool password: ");
+const url = testCredentials.url || readline.question("Enter the PowerSchool installation URL (such as: http://sales.powerschool.com): ");
+const username = testCredentials.username || readline.question("Enter your PowerSchool username: ");
+const password = testCredentials.password || readline.question("Enter your PowerSchool password: ");
 if(!url || !username || !password) return console.error("Invalid information entered.");
 
 // Create a new PowerSchool wrapper with our installation URL.
@@ -17,10 +23,13 @@ api.setup().then((api) => {
         // If user is null, invalid credentials were provided.
         if(!user) return console.error("Invalid credentials provided.");
         // Otherwise, let's get the student's information.
-        user.getStudentInfo().then((info) => {
-            // Log the user's courses to console.
-            console.log(`You're enrolled in ${info.courses.length} course${info.courses.length == 1 ? "" : "s"}${info.courses.length > 0 ? ":" : "."}`);
-            console.log(info.courses.map((course) => `- ${course.title}`).join("\n"));
+        user.getStudentsInfo().then((students) => {
+            students.forEach((info) => {
+                console.log(`Hey, ${info.student.getFormattedName()}!`);
+                // Log the user's courses to console.
+                console.log(`You're enrolled in ${info.courses.length} course${info.courses.length == 1 ? "" : "s"}${info.courses.length > 0 ? ":" : "."}`);
+                console.log(info.courses.sort((a, b) => a.expression.localeCompare(b.expression)).map((course) => `- ${course.title} (${course.getTeacher().getFormattedName()}) -- ${course.expression}`).join("\n"));
+            })
         }).catch((err) => console.error("Couldn't get more user info:", err))
     }).catch((err) => console.error("Couldn't login PowerSchool user:", err));
 }).catch((err) => console.error("Couldn't load PowerSchool API:", err));
